7c4eb14eb018010232611eb358a03666e0556dbc
tankorsmash <tankorsmash@gmail.com>
2021-07-15 11:33:24 -0400

    change to Browser.application by blindly hacking away

--- src/Main.elm
+++ src/Main.elm
@@ -8,9 +8,11 @@ module Main exposing (..)
 
 
 import Browser
+import Browser.Navigation as Nav
 import Html exposing (Html, button, div, text, input)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import Url
 
 import Debug --for prints
 
@@ -22,38 +24,45 @@ import Time
 
 
 main =
-    Browser.element {
+    -- Browser.element {
+    --     init = init, view = view, update = update2,
+    --     subscriptions = subscriptions
+    -- }
+    Browser.application {
         init = init, view = view, update = update2,
-        subscriptions = subscriptions
+        subscriptions = subscriptions,
+        onUrlChange = UrlChanged,
+        onUrlRequest = LinkClicked
     }
 
 
 
-subscriptions : Model -> Sub Msg
+subscriptions :  Model -> Sub Msg
 subscriptions model =
     Time.every 1000 Tick
 
 -- MODEL
 
 
-type alias Model = {
-    count : Int,
-    content : String,
-    time : Time.Posix,
-    zone : Time.Zone
+type alias Model =
+    { count : Int
+    , content : String
+    , time : Time.Posix
+    , zone : Time.Zone
+    , key : Nav.Key
+    , url : Url.Url
     }
 
 
-init : () -> (Model, Cmd Msg)
-init _ =
-    ( Model 0 "ASD" (Time.millisToPosix 0) Time.utc
+init : () -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
+init _ url key =
+    ( Model 0 "ASD" (Time.millisToPosix 0) Time.utc key url
     , Task.perform AdjustTimeZone Time.here
     )
 
 
 
 -- UPDATE
-
 
 
 type Msg
@@ -63,9 +72,11 @@ type Msg
     | Change String
     | Tick Time.Posix
     | AdjustTimeZone Time.Zone
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
 
 
-update2 : Msg -> Model -> (Model, Cmd Msg)
+update2 : Msg ->  Model -> (Model, Cmd Msg)
 update2 msg model =
     case msg of
         Increment ->
@@ -86,25 +97,37 @@ update2 msg model =
         AdjustTimeZone newZone ->
             ({ model | zone = newZone }, Cmd.none)
 
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    (model, Nav.pushUrl model.key (Url.toString url))
+                Browser.External href ->
+                    (model, Nav.load href)
+
+        UrlChanged url ->
+            ( {model | url = url }
+            , Cmd.none
+            )
+
 
 my_func : Int -> Int
 my_func age = age * 100
 
 
-custom_input : String -> String -> String -> (String -> msg) -> Html msg
-custom_input type__ placeholder_ value_ toMsg =
-    input [ type_ type__, placeholder placeholder_, value value_, onInput toMsg ] []
+-- custom_input : String -> String -> String -> (String -> msg) -> Html msg
+-- custom_input type__ placeholder_ value_ toMsg =
+--     input [ type_ type__, placeholder placeholder_, value value_, onInput toMsg ] []
 
 
-display_validation : String -> Html msg
-display_validation to_validate =
-    if to_validate == "valid" then
-        div [style "color" "green"] [text "MFer is valid" ]
-    else if True then
-        div [] [text "true"]
-    else
-        div [] [text ("try again" ++ (Debug.log ("to_validate: "++to_validate) ""))]
-
+-- display_validation : String -> Html msg
+-- display_validation to_validate =
+--     if to_validate == "valid" then
+--         div [style "color" "green"] [text "MFer is valid" ]
+--     else if True then
+--         div [] [text "true"]
+--     else
+--         div [] [text ("try again" ++ (Debug.log ("to_validate: "++to_validate) ""))]
+--
 
 humanize : Time.Posix -> Time.Zone -> String
 humanize time zone =
@@ -118,18 +141,22 @@ humanize time zone =
 -- VIEW
 
 
-view : Model -> Html Msg
+-- view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    div []
-    [
-        -- custom_input "text" "ppplllace" model.content Change
-        custom_input "text" "ppplllace" (humanize model.time model.zone) Change
-        , display_validation model.content
-    -- button [ onClick Decrement ] [ text "-" ]
-    -- , div [] [ text (String.fromInt model.count) ]
-    -- , button [ onClick Increment ] [ text "+" ]
-    -- , div [] [ text (model.content ++ ", the length is: "++ String.fromInt (String.length model.content))]
-    -- , div [] [ text (String.fromInt (my_func model.count))]
-    -- , input [ placeholder "placeholder", value model.content, onInput Change] []
-    ]
+    { title = "URL Interceptor"
+    , body =
+        [
+            text "ASDASD"
+            -- custom_input "text" "ppplllace" model.content Change
+            -- custom_input "text" "ppplllace" (humanize model.time model.zone) Change
+            -- , display_validation model.content
+            -- button [ onClick Decrement ] [ text "-" ]
+            -- , div [] [ text (String.fromInt model.count) ]
+            -- , button [ onClick Increment ] [ text "+" ]
+            -- , div [] [ text (model.content ++ ", the length is: "++ String.fromInt (String.length model.content))]
+            -- , div [] [ text (String.fromInt (my_func model.count))]
+            -- , input [ placeholder "placeholder", value model.content, onInput Change] []
+            ]
+    }
 

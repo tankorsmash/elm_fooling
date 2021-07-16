f166be4e92aa5c1b2d58a91fef23c9f3b005e91e
tankorsmash <tankorsmash@gmail.com>
2021-07-15 23:08:36 -0400

    use elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -5,38 +5,32 @@ module Main exposing (..)
 -- Read how it works:
 --   https://guide.elm-lang.org/architecture/buttons.html
 --
-
-
-import Browser
-import Browser.Navigation as Nav
-import Html exposing (Html, button, span, div, text, input, b, a, h1, h2, h3, h4, br)
 -- import Html exposing (..)
-import Html.Attributes exposing (href, style, property, attribute)
 -- import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
-import Url
-import Url.Parser exposing (Parser, (</>), int, map, oneOf, s, string, parse)
+--for prints
 
-import Http
-import Json.Encode exposing (string)
-import Json.Decode exposing (Decoder, field, string, list, at)
-
-
-import List
-
-import Debug --for prints
-
-import Task
-import Time
-
-import Bootstrap.CDN as CDN
-import Bootstrap.Grid as Grid
-import Bootstrap.Grid.Row as Row
-import Bootstrap.Grid.Col as Col
 import Bootstrap.Button as Button
+import Bootstrap.CDN as CDN
 import Bootstrap.Card as Card
 import Bootstrap.Form.Input as Input
 import Bootstrap.Form.InputGroup as InputGroup
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
+import Browser
+import Browser.Navigation as Nav
+import Debug
+import Html exposing (Html, a, b, br, button, div, h1, h2, h3, h4, input, span, text)
+import Html.Attributes exposing (attribute, href, property, style)
+import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode exposing (Decoder, at, field, list, string)
+import Json.Encode exposing (string)
+import List
+import Task
+import Time
+import Url
+import Url.Parser exposing ((</>), Parser, int, map, oneOf, parse, s, string)
 
 
 type Msg
@@ -48,11 +42,9 @@ type Msg
     | AdjustTimeZone Time.Zone
     | LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
-
     | DownloadAllPosts
     | GotJSON (Result Http.Error (List String))
-    | GotPostById (Result Http.Error (PostData))
-
+    | GotPostById (Result Http.Error PostData)
     | PostIDToDownloadChanged String
     | DownloadPostById Int
 
@@ -60,45 +52,59 @@ type Msg
 
 -- MAIN
 
-root_json_server_url = "http://localhost:5021/"
+
+root_json_server_url =
+    "http://localhost:5021/"
 
 
 add_class cls =
     property "className" (Json.Encode.string cls)
 
+
 bootstrap_button type_ on_click text_ =
-    Button.button [
-        type_,
-        Button.attrs [ onClick on_click]
-    ] [ text text_]
+    Button.button
+        [ type_
+        , Button.attrs [ onClick on_click ]
+        ]
+        [ text text_ ]
+
 
 button_primary on_click text_ =
     bootstrap_button Button.primary on_click text_
+
+
 button_secondary on_click text_ =
     bootstrap_button Button.secondary on_click text_
 
+
 empty_div : Html msg
-empty_div = div [] []
+empty_div =
+    div [] []
+
 
 main =
     -- Browser.element {
     --     init = init, view = view, update = update2,
     --     subscriptions = subscriptions
     -- }
-    Browser.application {
-        init = init, view = view, update = update2,
-        subscriptions = subscriptions,
-        onUrlChange = UrlChanged,
-        onUrlRequest = LinkClicked
-    }
-
+    Browser.application
+        { init = init
+        , view = view
+        , update = update2
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Time.every 1000 Tick
 
+
+
 -- MODEL
+
 
 type alias PostData =
     { id : Int
@@ -106,12 +112,14 @@ type alias PostData =
     , author : String
     }
 
+
 type alias PageInfo =
     { key : Nav.Key
     , url : Url.Url
     , route : Route
     , page : Page
     }
+
 
 type alias Model =
     { count : Int
@@ -122,8 +130,8 @@ type alias Model =
     , post_data : PostData
     , post_id_to_download : Int
     , post_id_to_download_err_status : Int
-
     }
+
 
 type Page
     = NotFoundPage
@@ -131,14 +139,15 @@ type Page
     | ProfilePage
     | UnsetPage
 
+
 type Route
     = Home
     | NotFound
     | Profile
-    -- | Topic String
-    -- | Blog Int
-    -- | User String
-    -- | Comment String Int
+      -- | Topic String
+      -- | Blog Int
+      -- | User String
+      -- | Comment String Int
     | UnsetRoute
 
 
@@ -147,14 +156,17 @@ parseUrl url =
     case parse matchRoute url of
         Just route ->
             route
+
         Nothing ->
             NotFound
+
 
 matchRoute : Parser (Route -> a) a
 matchRoute =
     oneOf
-        [ map Home       (s "home")
-        , map Profile    (s "profile")
+        [ map Home (s "home")
+        , map Profile (s "profile")
+
         -- , map Topic   (s "topic" </> string)
         -- , map Blog    (s "blog" </> int)
         -- , map User    (s "user" </> string)
@@ -169,13 +181,13 @@ download_post_by_id post_id =
         , expect = Http.expectJson GotPostById decode_single_post
         }
 
-decode_single_post : Decoder (PostData)
+
+decode_single_post : Decoder PostData
 decode_single_post =
     Json.Decode.map3 PostData
-        (at ["id"] Json.Decode.int)
-        (at ["title"] Json.Decode.string)
-        (at ["author"] Json.Decode.string)
-
+        (at [ "id" ] Json.Decode.int)
+        (at [ "title" ] Json.Decode.string)
+        (at [ "author" ] Json.Decode.string)
 
 
 download_all_posts : Cmd Msg
@@ -185,27 +197,37 @@ download_all_posts =
         , expect = Http.expectJson GotJSON decode_post_titles
         }
 
+
 decode_post_titles : Decoder (List String)
 decode_post_titles =
     list (field "title" Json.Decode.string)
 
-init : () -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url navKey =
     let
-        parsedRoute = parseUrl url
-        page_info = PageInfo navKey url (parseUrl url) UnsetPage
-        post_data = PostData -1 "No Name" "No Title"
-        model = Model 0 "ASD" (Time.millisToPosix 0) Time.utc page_info post_data -1 0
+        parsedRoute =
+            parseUrl url
 
-        existingCmds = Task.perform AdjustTimeZone Time.here
+        page_info =
+            PageInfo navKey url (parseUrl url) UnsetPage
+
+        post_data =
+            PostData -1 "No Name" "No Title"
+
+        model =
+            Model 0 "ASD" (Time.millisToPosix 0) Time.utc page_info post_data -1 0
+
+        existingCmds =
+            Task.perform AdjustTimeZone Time.here
     in
-        initCurrentPage (model, existingCmds)
+    initCurrentPage ( model, existingCmds )
 
 
-initCurrentPage : (Model, Cmd Msg) -> (Model, Cmd Msg)
-initCurrentPage (model, existingCmds) =
+initCurrentPage : ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
+initCurrentPage ( model, existingCmds ) =
     let
-        ( currentPage ,mappedPageCmds ) =
+        ( currentPage, mappedPageCmds ) =
             case model.page_info.route of
                 Home ->
                     ( HomePage, Cmd.none )
@@ -218,146 +240,173 @@ initCurrentPage (model, existingCmds) =
 
                 UnsetRoute ->
                     ( NotFoundPage, Cmd.none )
-        page_info = model.page_info
+
+        page_info =
+            model.page_info
     in
-    ({model | page_info = {page_info | page = currentPage}}
+    ( { model | page_info = { page_info | page = currentPage } }
     , Cmd.batch [ existingCmds, mappedPageCmds ]
     )
-
 
 
 
 -- UPDATE
 
 
-update2 : Msg ->  Model -> (Model, Cmd Msg)
+update2 : Msg -> Model -> ( Model, Cmd Msg )
 update2 msg model =
     case msg of
         Increment ->
-            ({ model | count = model.count + 10}, Cmd.none)
+            ( { model | count = model.count + 10 }, Cmd.none )
 
         Decrement ->
-            ({ model | count = model.count - 1}, Cmd.none)
+            ( { model | count = model.count - 1 }, Cmd.none )
 
         Poop ->
-            ({ model | count = model.count + 2}, Cmd.none)
+            ( { model | count = model.count + 2 }, Cmd.none )
 
         Change newContent ->
-            ({ model | content = newContent }, Cmd.none)
+            ( { model | content = newContent }, Cmd.none )
 
         Tick newTime ->
-            ({ model | time = newTime }, Cmd.none)
+            ( { model | time = newTime }, Cmd.none )
 
         AdjustTimeZone newZone ->
-            ({ model | zone = newZone }, Cmd.none)
+            ( { model | zone = newZone }, Cmd.none )
 
         LinkClicked urlRequest ->
             case urlRequest of
                 Browser.Internal url ->
-                    (model, Nav.pushUrl model.page_info.key (Url.toString url))
+                    ( model, Nav.pushUrl model.page_info.key (Url.toString url) )
+
                 Browser.External href ->
-                    (model, Nav.load href)
+                    ( model, Nav.load href )
 
         UrlChanged url ->
             let
-                page_info = model.page_info
-                newRoute = parseUrl url
+                page_info =
+                    model.page_info
+
+                newRoute =
+                    parseUrl url
             in
-                ( { model | page_info = { page_info | url = url, route=newRoute } }
-                , Cmd.none
-                ) |> initCurrentPage
+            ( { model | page_info = { page_info | url = url, route = newRoute } }
+            , Cmd.none
+            )
+                |> initCurrentPage
 
         DownloadAllPosts ->
-            (model, download_all_posts)
+            ( model, download_all_posts )
 
         GotJSON result ->
             case result of
                 Ok titles ->
                     let
-                        post_data = Debug.log "Successfully received files!" model.post_data
-                        title = List.head titles
+                        post_data =
+                            Debug.log "Successfully received files!" model.post_data
+
+                        title =
+                            List.head titles
                     in
-                        case title of
-                            Just title_ ->
-                                ({model | post_data = { post_data | title=title_ }}, Cmd.none)
-                            Nothing ->
-                                ({model | post_data = { post_data | title="Error" }}, Cmd.none)
+                    case title of
+                        Just title_ ->
+                            ( { model | post_data = { post_data | title = title_ } }, Cmd.none )
+
+                        Nothing ->
+                            ( { model | post_data = { post_data | title = "Error" } }, Cmd.none )
 
                 Err error ->
                     case error of
                         Http.BadBody err_msg ->
-                            (Debug.log err_msg model, Cmd.none)
+                            ( Debug.log err_msg model, Cmd.none )
+
                         _ ->
-                            (Debug.log ("Unknown error downloading") model, Cmd.none)
+                            ( Debug.log "Unknown error downloading" model, Cmd.none )
+
         GotPostById result ->
             case result of
                 Ok new_post_data ->
                     let
-                        existing_post_data = Debug.log "Successfully received files!" model.post_data
+                        existing_post_data =
+                            Debug.log "Successfully received files!" model.post_data
                     in
-                        ({model | post_data = new_post_data, post_id_to_download_err_status = 0 }, Cmd.none)
+                    ( { model | post_data = new_post_data, post_id_to_download_err_status = 0 }, Cmd.none )
 
                 Err error ->
                     case error of
                         Http.BadBody err_msg ->
-                            (Debug.log err_msg model, Cmd.none)
+                            ( Debug.log err_msg model, Cmd.none )
+
                         Http.BadStatus status ->
-                            (Debug.log
-                                ("Received a bad status: " ++ (String.fromInt status))
+                            ( Debug.log
+                                ("Received a bad status: " ++ String.fromInt status)
                                 { model | post_id_to_download_err_status = status }
-                             , Cmd.none)
+                            , Cmd.none
+                            )
+
                         _ ->
-                            (Debug.log ("Unknown error downloading") model, Cmd.none)
+                            ( Debug.log "Unknown error downloading" model, Cmd.none )
 
         PostIDToDownloadChanged strPostId ->
             let
-                maybePostId = String.toInt strPostId
+                maybePostId =
+                    String.toInt strPostId
             in
-                case maybePostId of
-                    Just intPostId ->
-                        ({ model | post_id_to_download = intPostId}, Cmd.none)
-                    Nothing ->
-                        Debug.log "couldnt convert to int" ( model, Cmd.none)
+            case maybePostId of
+                Just intPostId ->
+                    ( { model | post_id_to_download = intPostId }, Cmd.none )
+
+                Nothing ->
+                    Debug.log "couldnt convert to int" ( model, Cmd.none )
 
         DownloadPostById post_id_to_download_ ->
-            (model, download_post_by_id post_id_to_download_)
+            ( model, download_post_by_id post_id_to_download_ )
 
 
 humanize : Time.Posix -> Time.Zone -> String
 humanize time zone =
     let
-        hour = String.fromInt (Time.toHour zone time)
-        minute = String.fromInt (Time.toMinute zone time)
-        second = String.fromInt (Time.toSecond zone time)
+        hour =
+            String.fromInt (Time.toHour zone time)
+
+        minute =
+            String.fromInt (Time.toMinute zone time)
+
+        second =
+            String.fromInt (Time.toSecond zone time)
     in
     hour ++ ":" ++ minute ++ ":" ++ second
 
+
+
 -- VIEW
+
 
 navigation : Model -> Html Msg
 navigation model =
-    div [ style "margin-bottom" "15px"]
-    [ div []
-        [ text "This is a link: "
-        , a [href "/home" ] [ text "HOME" ]
+    div [ style "margin-bottom" "15px" ]
+        [ div []
+            [ text "This is a link: "
+            , a [ href "/home" ] [ text "HOME" ]
+            ]
+        , div []
+            [ text "This is my profile: "
+            , a [ href "/profile" ] [ text "PROFILE" ]
+            ]
         ]
-    , div []
-        [ text "This is my profile: "
-        , a [href "/profile" ] [ text "PROFILE" ]
-        ]
-    ]
+
 
 homeView : Model -> Html Msg
 homeView model =
     div [ add_class "container" ]
-        [ navigation  model
+        [ navigation model
         , CDN.stylesheet
-        , h3 [] [text "HOME PAGE IS HERE!!!"]
-        , div [] [
-            text <| "Post data -- Title: " ++ model.post_data.title ++ ", and Author: " ++ model.post_data.author
+        , h3 [] [ text "HOME PAGE IS HERE!!!" ]
+        , div []
+            [ text <| "Post data -- Title: " ++ model.post_data.title ++ ", and Author: " ++ model.post_data.author
             ]
-        , div [] [
-            Button.button
+        , div []
+            [ Button.button
                 [ Button.primary
                 , Button.attrs [ onClick DownloadAllPosts ]
                 ]
@@ -366,27 +415,30 @@ homeView model =
         , br [] []
         , div []
             [ Grid.row []
-                [ Grid.col [Col.lg3]
+                [ Grid.col [ Col.lg3 ]
                     [ InputGroup.config
-                        ( InputGroup.number
+                        (InputGroup.number
                             [ Input.placeholder "post_id"
                             , Input.value (String.fromInt model.post_id_to_download)
                             , Input.onInput PostIDToDownloadChanged
-                            ])
+                            ]
+                        )
                         |> InputGroup.predecessors
                             [ InputGroup.span [] [ text "Post ID" ] ]
                         |> InputGroup.view
                     ]
-                , Grid.col [Col.lg6]
+                , Grid.col [ Col.lg6 ]
                     [ button_secondary (DownloadPostById model.post_id_to_download) "Download Entire PostData" ]
                 ]
-                , case model.post_id_to_download_err_status of
-                    0 ->
-                        empty_div
-                    _ ->
-                        div [style "color" "red" ] [ text <| "ERROR STATUS: " ++ (String.fromInt model.post_id_to_download_err_status)]
+            , case model.post_id_to_download_err_status of
+                0 ->
+                    empty_div
+
+                _ ->
+                    div [ style "color" "red" ] [ text <| "ERROR STATUS: " ++ String.fromInt model.post_id_to_download_err_status ]
             ]
         ]
+
 
 profileView : Model -> Html Msg
 profileView model =
@@ -396,15 +448,17 @@ profileView model =
         ]
 
 
+
 -- view : Model -> Html Msg
+
+
 view : Model -> Browser.Document Msg
 view model =
     { title = "URL Interceptor"
     , body =
-        [
-        case model.page_info.page of
+        [ case model.page_info.page of
             NotFoundPage ->
-                div [] [ text "Not found page"]
+                div [] [ text "Not found page" ]
 
             HomePage ->
                 homeView model
@@ -413,7 +467,6 @@ view model =
                 profileView model
 
             UnsetPage ->
-                div [] [ text "UNSET PAGE"]
+                div [] [ text "UNSET PAGE" ]
         ]
     }
-

e5c25db0a205e0c90ab760203feb1746f424f2f5
tankorsmash <tankorsmash@gmail.com>
2021-08-04 13:34:20 -0400

    cache open dota player profile response so i dont need to worry about api ratelimiting

--- src/opendota/OpenDota.elm
+++ src/opendota/OpenDota.elm
@@ -5,6 +5,8 @@ import Json.Decode as Decode exposing (Decoder, field, int, list, string)
 import Json.Decode.Pipeline exposing (hardcoded, optional, optionalAt, required, requiredAt)
 import Result
 
+root_json_server_url =
+    "http://localhost:5021/"
 
 root_url : String
 root_url =
@@ -43,6 +45,7 @@ decode_player_data =
 download_player_data : (Int) -> (Result Http.Error PlayerData -> msg) -> Cmd msg
 download_player_data account_id the_msg =
     Http.get
-        { url = root_url ++ "players/" ++ String.fromInt account_id
+        -- { url = root_url ++ "players/" ++ String.fromInt account_id
+        { url = root_json_server_url ++ "open_dota_player_data"
         , expect = Http.expectJson the_msg decode_player_data
         }

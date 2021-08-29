9777e81700f92de21c601eb63caec75e5b4b5cc2
tankorsmash <tankorsmash@gmail.com>
2021-08-28 22:26:27 -0400

    show a fullscreen modal if you press a button

--- src/ElmUIPlayground.elm
+++ src/ElmUIPlayground.elm
@@ -2,9 +2,10 @@ module ElmUIPlayground exposing (Model, Msg, init, update, view)
 
 import Color
 import Color.Convert as Convert
-import Element exposing (Color, Element, alignLeft, alignRight, centerX, centerY, column, el, explain, fill, fillPortion, padding, rgb, rgb255, row, spacing, text, width)
+import Element exposing (Color, Element, alignLeft, alignRight, centerX, centerY, column, el, explain, fill, fillPortion, height, padding, rgb, rgb255, row, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border
+import Element.Events as Events
 import Element.Font as Font
 import Element.Input as Input
 import Html
@@ -13,10 +14,11 @@ import Html
 type Msg
     = Click
     | Increment
+    | ShowModal (Maybe String)
 
 
 type alias Model =
-    { rounded_edges : Int }
+    { rounded_edges : Int, show_modal : Bool, modal_text : Maybe String }
 
 
 white_color : Color
@@ -42,7 +44,10 @@ primary_color =
 
 init : Model
 init =
-    { rounded_edges = 3 }
+    { rounded_edges = 3
+    , show_modal = False
+    , modal_text = Nothing
+    }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -53,6 +58,9 @@ update msg model =
 
         Increment ->
             ( { model | rounded_edges = model.rounded_edges + 10 }, Cmd.none )
+
+        ShowModal maybe_str ->
+            ( { model | show_modal = not model.show_modal, modal_text = maybe_str }, Cmd.none )
 
 
 primary_button : List (Element.Attribute Msg) -> Msg -> String -> Element Msg
@@ -73,15 +81,49 @@ primary_button attrs on_press label =
         { onPress = Just on_press, label = text label }
 
 
+modal maybe_str =
+    let
+        str =
+            case maybe_str of
+                Just str_ ->
+                    str_
+
+                Nothing ->
+                    "IN FRONT"
+    in
+    row
+        [ width fill
+        , height fill
+        , Font.center
+        , Background.color <| rgb 1 0 0
+        , Events.onClick <| ShowModal Nothing
+
+        -- , Element.explain Debug.todo
+        ]
+        [ column [ centerX ] [ text str ] ]
+
+
 view : Model -> Html.Html Msg
 view model =
-    Element.layout [] <|
+    let
+        in_front =
+            if model.show_modal then
+                Element.inFront <| modal model.modal_text
+
+            else
+                -- TODO come up with a better way to return nothing from this
+                Element.inFront <| el [] <| text ""
+    in
+    Element.layout [ in_front ] <|
         column [ width fill, centerY ]
             [ myRowOfStuff model
             , row [ width fill ]
                 [ row [ width <| fillPortion 2 ] []
                 , column [ width <| fillPortion 1 ]
-                    [ primary_button [] Increment "Click Me"
+                    [ primary_button [] (ShowModal Nothing) "Click Me"
+                    ]
+                , column [ width <| fillPortion 1 ]
+                    [ primary_button [] (ShowModal <| Just "OMFG \nModal Text!!!") "Modal With Text"
                     ]
                 , column [ width <| fillPortion 1 ]
                     [ primary_button [] Increment "End"

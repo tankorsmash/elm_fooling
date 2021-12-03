6fdc0cdbf17a194eedf104ef515b0056a4a1817e
tankorsmash <tankorsmash@gmail.com>
2021-12-02 22:59:04 -0500

    add tests for adding and removing from the average

--- tests/Example.elm
+++ tests/Example.elm
@@ -10,11 +10,38 @@ suite : Test
 suite =
     -- todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
     describe "Basic math check for changing averages"
-        [ test "Adding nothing changes nothing" <|
+        [ test "Adding nothing changes nothing in average" <|
             \_ ->
                 let
                     orig_avg =
                         10
                 in
-                Expect.equal orig_avg orig_avg
+                Expect.equal orig_avg (add_to_average orig_avg 1 0 0)
+        , test "Adding a single item works" <|
+            \_ ->
+                let
+                    orig_avg =
+                        10
+
+                    orig_num =
+                        1
+                in
+                Expect.equal 15 (add_to_average orig_avg orig_num 20 1)
+        , test "Removing nothing changes nothing in average" <|
+            \_ ->
+                let
+                    orig_avg =
+                        10
+                in
+                Expect.equal orig_avg (sub_from_average orig_avg 1 0 0)
+        , test "Removing a single item changes nothing in average" <|
+            \_ ->
+                let
+                    orig_avg =
+                        20
+
+                    orig_num =
+                        2
+                in
+                Expect.equal 30 (sub_from_average orig_avg orig_num 10 1)
         ]

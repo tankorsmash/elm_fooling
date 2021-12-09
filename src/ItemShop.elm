b552606bf1f7e80418b2cad497bed6c047e645c0
tankorsmash <tankorsmash@gmail.com>
2021-12-09 17:22:03 -0500

    hook up basic test

--- src/ItemShop.elm
+++ src/ItemShop.elm
@@ -3483,39 +3483,46 @@ special_actions_display model =
 suite : Test
 suite =
     -- todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
-    describe "Basic math check for changing averages"
-        [ test "Adding nothing changes nothing in average" <|
-            \_ ->
-                let
-                    orig_avg =
-                        10
-                in
-                Expect.equal orig_avg (add_to_average orig_avg 1 0 0)
-        , test "Adding a single item works to change the average" <|
-            \_ ->
-                let
-                    orig_avg =
-                        10
+    describe "root test suite"
+        [ describe "Basic math check for changing averages"
+            [ test "Adding nothing changes nothing in average" <|
+                \_ ->
+                    let
+                        orig_avg =
+                            10
+                    in
+                    Expect.equal orig_avg (add_to_average orig_avg 1 0 0)
+            , test "Adding a single item works to change the average" <|
+                \_ ->
+                    let
+                        orig_avg =
+                            10
 
-                    orig_num =
-                        1
-                in
-                Expect.equal 15 (add_to_average orig_avg orig_num 20 1)
-        , test "Removing nothing changes nothing in average" <|
-            \_ ->
-                let
-                    orig_avg =
-                        10
-                in
-                Expect.equal orig_avg (sub_from_average orig_avg 1 0 0)
-        , test "Removing a single item changes the average as expected" <|
-            \_ ->
-                let
-                    orig_avg =
-                        20
+                        orig_num =
+                            1
+                    in
+                    Expect.equal 15 (add_to_average orig_avg orig_num 20 1)
+            , test "Removing nothing changes nothing in average" <|
+                \_ ->
+                    let
+                        orig_avg =
+                            10
+                    in
+                    Expect.equal orig_avg (sub_from_average orig_avg 1 0 0)
+            , test "Removing a single item changes the average as expected" <|
+                \_ ->
+                    let
+                        orig_avg =
+                            20
 
-                    orig_num =
-                        2
-                in
-                Expect.equal 30 (sub_from_average orig_avg orig_num 10 1)
+                        orig_num =
+                            2
+                    in
+                    Expect.equal 30 (sub_from_average orig_avg orig_num 10 1)
+            ]
+        , describe "Item Shop stuff"
+            [ test "clipText test" <|
+                \_ ->
+                    Expect.equal "abc..." <| clipText "abcdef" 3
+            ]
         ]

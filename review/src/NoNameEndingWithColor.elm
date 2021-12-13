3d61cb14eda03c7a82794b199e6b397dff47bfcf
tankorsmash <tankorsmash@gmail.com>
2021-12-13 01:31:16 -0500

    rework rule to pass along errors as context and build up the final error at the end

--- review/src/NoNameEndingWithColor.elm
+++ review/src/NoNameEndingWithColor.elm
@@ -1,10 +1,12 @@
 module NoNameEndingWithColor exposing (rule)
 
+import Dict
 import Elm.Syntax.Declaration as Declaration exposing (Declaration)
 import Elm.Syntax.Exposing as Exposing
 import Elm.Syntax.Expression as Expression exposing (Expression)
 import Elm.Syntax.Module as Module exposing (Module)
 import Elm.Syntax.Node as Node exposing (Node)
+import Elm.Syntax.Range exposing (Range)
 import Review.Fix as Fix
 import Review.Rule as Rule exposing (Direction, Error, Rule)
 
@@ -18,9 +20,24 @@ stub =
     123
 
 
-type ExposedFunctions
-    = All
-    | OnlySome (List String)
+{-| dict of func names we're replacing
+-}
+type alias AllFixesToApply =
+    Dict.Dict String Fixes
+
+
+type alias FixesToApply =
+    ( String, Fixes )
+
+
+type alias Fixes =
+    List ( Range, List Fix.Fix )
+
+
+
+-- type AllFixesToApply
+--     = All
+--     | OnlySome (List String)
 
 
 {-| Reports... REPLACEME
@@ -60,12 +77,11 @@ elm-review --template undefined/example --rules NoNameEndingWithColor
 rule : Rule
 rule =
     -- Rule.newModuleRuleSchema "NoNameEndingWithColor" ()
-    Rule.newModuleRuleSchema "NoNameEndingWithColor" (OnlySome [])
+    Rule.newModuleRuleSchema "NoNameEndingWithColor" Dict.empty
         -- Add your visitors
         |> Rule.withModuleDefinitionVisitor moduleDefinitionVisitor
-        -- |> Rule.withSimpleDeclarationVisitor declarationVisitor
         |> Rule.withDeclarationVisitor declarationVisitor
-        -- |> Rule.withSimpleExpressionVisitor expressionVisitor
+        |> Rule.withFinalModuleEvaluation finalEvaluation
         |> Rule.fromModuleRuleSchema
 
 
@@ -79,7 +95,7 @@ fix_color_str str =
             |> String.append "color_"
 
 
-validate_exposing_node : Node Exposing.TopLevelExpose -> List (Error {})
+validate_exposing_node : Node Exposing.TopLevelExpose -> Maybe FixesToApply
 validate_exposing_node exposed_val =
     case Node.value exposed_val of
         Exposing.FunctionExpose functionNameStr ->
@@ -89,33 +105,35 @@ validate_exposing_node exposed_val =
             in
             if String.endsWith "_color" functionNameStr then
                 -- Return a single error, describing the problem
-                [ Rule.errorWithFix
-                    { message = "Exposed names should start with color_, not end with it"
-                    , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
-                    }
-                    -- This is the location of the problem in the source code
-                    node_range
-                    [ Fix.replaceRangeBy node_range (fix_color_str functionNameStr) ]
-                ]
+                -- [ Rule.errorWithFix
+                --     { message = "Exposed names should start with color_, not end with it"
+                --     , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+                --     }
+                --     -- This is the location of the problem in the source code
+                --     node_range
+                --     [ Fix.replaceRangeBy node_range (fix_color_str functionNameStr) ]
+                -- ]
+                Just ( functionNameStr, [ ( node_range, [ Fix.replaceRangeBy node_range (fix_color_str functionNameStr) ] ) ] )
 
             else
-                []
+                Nothing
 
         Exposing.InfixExpose _ ->
-            []
+            Nothing
 
         Exposing.TypeOrAliasExpose _ ->
-            []
+            Nothing
 
         Exposing.TypeExpose _ ->
-            []
+            Nothing
 
 
-declarationVisitor : Node Declaration -> Direction -> ExposedFunctions -> ( List (Error {}), ExposedFunctions )
+declarationVisitor : Node Declaration -> Direction -> AllFixesToApply -> ( List (Error {}), AllFixesToApply )
 declarationVisitor node direction context =
     case ( direction, Node.value node ) of
         ( Rule.OnEnter, Declaration.FunctionDeclaration { signature, documentation, declaration } ) ->
             let
+                signatureError : Fixes
                 signatureError =
                     case signature of
                         Just sig_node ->
@@ -127,14 +145,7 @@ declarationVisitor node direction context =
                                             sig_name_range =
                                                 Node.range sig_name_node
                                         in
-                                        [ Rule.errorWithFix
-                                            { message = "Names in signatures should start with color_, not end with it"
-                                            , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
-                                            }
-                                            -- This is the location of the problem in the source code
-                                            sig_name_range
-                                            [ Fix.replaceRangeBy sig_name_range (fix_color_str functionNameStr) ]
-                                        ]
+                                        [ ( sig_name_range, [ Fix.replaceRangeBy sig_name_range (fix_color_str functionNameStr) ] ) ]
                                    )
 
                         Nothing ->
@@ -151,16 +162,33 @@ declarationVisitor node direction context =
                     Node.range functionName
             in
             if String.endsWith "_color" functionNameStr then
-                ( [ Rule.errorWithFix
-                        { message = "Function names should start with color_, not end with it"
-                        , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
-                        }
-                        -- This is the location of the problem in the source code
-                        node_range
-                        [ Fix.replaceRangeBy node_range (fix_color_str functionNameStr) ]
-                  ]
-                    ++ signatureError
-                , context
+                -- ( [ Rule.errorWithFix
+                --         { message = "Function names should start with color_, not end with it"
+                --         , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+                --         }
+                --         -- This is the location of the problem in the source code
+                --         node_range
+                --         [ Fix.replaceRangeBy node_range (fix_color_str functionNameStr) ]
+                --   ]
+                --     ++ signatureError
+                -- , context
+                -- )
+                ( []
+                , Dict.update
+                    functionNameStr
+                    (\mb_existing_fixes ->
+                        let
+                            new_entry =
+                                [ ( node_range, [ Fix.replaceRangeBy node_range (fix_color_str functionNameStr) ] ) ] ++ signatureError
+                        in
+                        case mb_existing_fixes of
+                            Nothing ->
+                                Just new_entry
+
+                            Just existing_fixes ->
+                                Just <| List.append existing_fixes new_entry
+                    )
+                    context
                 )
 
             else
@@ -170,39 +198,78 @@ declarationVisitor node direction context =
             ( [], context )
 
 
-isExposed : ExposedFunctions -> String -> Bool
-isExposed exposedFunctions name =
-    case exposedFunctions of
-        All ->
-            True
-
-        OnlySome exposedList ->
-            List.member name exposedList
-
-
-moduleDefinitionVisitor : Node Module -> ExposedFunctions -> ( List (Error {}), ExposedFunctions )
+moduleDefinitionVisitor : Node Module -> AllFixesToApply -> ( List (Error {}), AllFixesToApply )
 moduleDefinitionVisitor node context =
     case Node.value node |> Module.exposingList of
         Exposing.All _ ->
-            ( [], All )
+            ( [], context )
 
         Exposing.Explicit exposedValues ->
-            ( let
-                color_errors =
-                    List.map
+            ( []
+            , let
+                fixes_to_apply : List ( String, List ( Range, List Fix.Fix ) )
+                fixes_to_apply =
+                    List.filterMap
                         validate_exposing_node
                         exposedValues
               in
-              List.concatMap identity color_errors |> List.reverse
-            , OnlySome (List.filterMap exposedFunctionName exposedValues)
+              List.foldl
+                (\( name, fixes ) c ->
+                    Dict.update
+                        name
+                        (\m ->
+                            case m of
+                                Nothing ->
+                                    Just fixes
+
+                                Just existing_fixes ->
+                                    Just <| List.append existing_fixes fixes
+                        )
+                        c
+                )
+                context
+                fixes_to_apply
+              -- fixes_to_apply
+              --   |> List.concatMap identity
+              --   |> List.reverse
+              --   |> List.append context
             )
 
 
-exposedFunctionName : Node Exposing.TopLevelExpose -> Maybe String
-exposedFunctionName value =
-    case Node.value value of
-        Exposing.FunctionExpose functionName ->
-            Just functionName
+fixes_to_apply_to_error : FixesToApply -> List (Error {})
+fixes_to_apply_to_error ( functionNameStr, range_fixes ) =
+    case range_fixes of
+        ( first_range, first_fixes ) :: rest_range_and_fixes ->
+            [ Rule.errorWithFix
+                { message = "Exposed names should start with color_, not end with it"
+                , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+                }
+                -- This is the location of the problem in the source code
+                first_range
+                (first_fixes ++ (List.concatMap identity <| List.map Tuple.second rest_range_and_fixes))
+            ]
 
-        _ ->
-            Nothing
+        [] ->
+            []
+
+
+finalEvaluation : AllFixesToApply -> List (Error {})
+finalEvaluation context =
+    context
+        |> Dict.toList
+        |> List.map
+            fixes_to_apply_to_error
+        |> List.concatMap identity
+
+
+
+-- case ( Dict.get [ "Element" ] context, Dict.get [ "Html", "Styled" ] context ) of
+--     ( Just elmUiRange, Just _ ) ->
+--         [ Rule.error
+--             { message = "Do not use both `elm-ui` and `elm-css`"
+--             , details = [ "At fruits.com, we use `elm-ui` in the dashboard application, and `elm-css` in the rest of the code. We want to use `elm-ui` in our new projects, but in projects using `elm-css`, we don't want to use both libraries to keep things simple." ]
+--             }
+--             elmUiRange
+--         ]
+--     _ ->
+--         []
--- review/tests/NoNameEndingWithColorTest.elm
+++ review/tests/NoNameEndingWithColorTest.elm
@@ -24,71 +24,87 @@ red_color = 123"""
                             |> Review.Test.atExactly { start = { row = 1, column = 29 }, end = { row = 1, column = 38 } }
                             |> Review.Test.whenFixed """module SomeModule exposing (color_red)
 
-red_color : Int
-red_color = 123"""
-                        , Review.Test.error
-                            { message = "Names in signatures should start with color_, not end with it"
-                            , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
-                            , under = "red_color"
-                            }
-                            |> Review.Test.atExactly { start = { row = 3, column = 1 }, end = { row = 3, column = 10 } }
-                            |> Review.Test.whenFixed """module SomeModule exposing (red_color)
-
 color_red : Int
-red_color = 123"""
-                        , Review.Test.error
-                            { message = "Function names should start with color_, not end with it"
-                            , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
-                            , under = "red_color"
-                            }
-                            |> Review.Test.atExactly { start = { row = 4, column = 1 }, end = { row = 4, column = 10 } }
-                            |> Review.Test.whenFixed """module SomeModule exposing (red_color)
-
-red_color : Int
 color_red = 123"""
                         ]
-        , test "should report an error when seeing a function or value ending with `_color`" <|
+        , test "Should not report an error at all" <|
             \() ->
                 """module SomeModule exposing (nothing)
 
 nothing = Nothing
-red_color = 123"""
+color_green = 123
+
+color_primary : Color
+color_primary = rgb 0 0 0"""
                     |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "Function names should start with color_, not end with it"
-                            , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
-                            , under = "red_color"
-                            }
-                            |> Review.Test.whenFixed """module SomeModule exposing (nothing)
-
-nothing = Nothing
-color_red = 123"""
-                        ]
-        , test "name with color in a module exposing should be replaced too" <|
-            \() ->
-                """module SomeModule exposing (red_color)
-
-red_color = 123"""
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "Function names should start with color_, not end with it"
-                            , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
-                            , under = "red_color"
-                            }
-                            |> Review.Test.atExactly { start = { row = 3, column = 1 }, end = { row = 3, column = 10 } }
-                            |> Review.Test.whenFixed """module SomeModule exposing (red_color)
-
-color_red = 123"""
-                        , Review.Test.error
-                            { message = "Exposed names should start with color_, not end with it"
-                            , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
-                            , under = "red_color"
-                            }
-                            |> Review.Test.atExactly { start = { row = 1, column = 29 }, end = { row = 1, column = 38 } }
-                            |> Review.Test.whenFixed """module SomeModule exposing (color_red)
-
-red_color = 123"""
-                        ]
+                    |> Review.Test.expectNoErrors
         ]
+
+
+
+--                         , Review.Test.error
+--                             { message = "Names in signatures should start with color_, not end with it"
+--                             , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+--                             , under = "red_color"
+--                             }
+--                             |> Review.Test.atExactly { start = { row = 3, column = 1 }, end = { row = 3, column = 10 } }
+--                             |> Review.Test.whenFixed """module SomeModule exposing (red_color)
+--
+-- color_red : Int
+-- red_color = 123"""
+--                         , Review.Test.error
+--                             { message = "Function names should start with color_, not end with it"
+--                             , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+--                             , under = "red_color"
+--                             }
+--                             |> Review.Test.atExactly { start = { row = 4, column = 1 }, end = { row = 4, column = 10 } }
+--                             |> Review.Test.whenFixed """module SomeModule exposing (red_color)
+--
+-- red_color : Int
+-- color_red = 123"""
+--                         ]
+--         , test "should report an error when seeing a function or value ending with `_color`" <|
+--             \() ->
+--                 """module SomeModule exposing (nothing)
+--
+-- nothing = Nothing
+-- red_color = 123"""
+--                     |> Review.Test.run rule
+--                     |> Review.Test.expectErrors
+--                         [ Review.Test.error
+--                             { message = "Function names should start with color_, not end with it"
+--                             , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+--                             , under = "red_color"
+--                             }
+--                             |> Review.Test.whenFixed """module SomeModule exposing (nothing)
+--
+-- nothing = Nothing
+-- color_red = 123"""
+--                         ]
+--         , test "name with color in a module exposing should be replaced too" <|
+--             \() ->
+--                 """module SomeModule exposing (red_color)
+--
+-- red_color = 123"""
+--                     |> Review.Test.run rule
+--                     |> Review.Test.expectErrors
+--                         [ Review.Test.error
+--                             { message = "Function names should start with color_, not end with it"
+--                             , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+--                             , under = "red_color"
+--                             }
+--                             |> Review.Test.atExactly { start = { row = 3, column = 1 }, end = { row = 3, column = 10 } }
+--                             |> Review.Test.whenFixed """module SomeModule exposing (red_color)
+--
+-- color_red = 123"""
+--                         , Review.Test.error
+--                             { message = "Exposed names should start with color_, not end with it"
+--                             , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+--                             , under = "red_color"
+--                             }
+--                             |> Review.Test.atExactly { start = { row = 1, column = 29 }, end = { row = 1, column = 38 } }
+--                             |> Review.Test.whenFixed """module SomeModule exposing (color_red)
+--
+-- red_color = 123"""
+--                         ]
+--      ]

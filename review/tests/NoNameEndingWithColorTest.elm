698d659258a45bbb7d356aed037b4063e6168dfa
tankorsmash <tankorsmash@gmail.com>
2021-12-12 23:44:32 -0500

    reenable original test

--- review/tests/NoNameEndingWithColorTest.elm
+++ review/tests/NoNameEndingWithColorTest.elm
@@ -8,27 +8,26 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoNameEndingWithColor"
-        --         [ test "should report an error when seeing a function or value ending with `_color`" <|
-        --             \() ->
-        --                 """module SomeModule exposing (nothing)
-        --
-        -- nothing = Nothing
-        -- red_color = 123"""
-        --                     |> Review.Test.run rule
-        --                     -- |> Review.Test.expectNoErrors
-        --                     |> Review.Test.expectErrors
-        --                         [ Review.Test.error
-        --                             { message = "Name should start with color_, not end with it"
-        --                             , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
-        --                             , under = "red_color"
-        --                             }
-        --                             |> Review.Test.whenFixed """module SomeModule exposing (nothing)
-        --
-        -- nothing = Nothing
-        -- color_red = 123"""
-        --                         ]
-        --      , test "name with color in a module exposing should be replaced too" <|
-        [ test "name with color in a module exposing should be replaced too" <|
+        [ test "should report an error when seeing a function or value ending with `_color`" <|
+            \() ->
+                """module SomeModule exposing (nothing)
+
+nothing = Nothing
+red_color = 123"""
+                    |> Review.Test.run rule
+                    -- |> Review.Test.expectNoErrors
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Name should start with color_, not end with it"
+                            , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+                            , under = "red_color"
+                            }
+                            |> Review.Test.whenFixed """module SomeModule exposing (nothing)
+
+nothing = Nothing
+color_red = 123"""
+                        ]
+        , test "name with color in a module exposing should be replaced too" <|
             \() ->
                 """module SomeModule exposing (red_color)
 

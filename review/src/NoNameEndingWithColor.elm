dde85ed56fa997118c31e29d3d45371577cada68
tankorsmash <tankorsmash@gmail.com>
2021-12-12 22:24:56 -0500

    fix custom elm-review rule to actually fail the test
    this is fairly tedious to set up

--- review/src/NoNameEndingWithColor.elm
+++ review/src/NoNameEndingWithColor.elm
@@ -1,5 +1,6 @@
 module NoNameEndingWithColor exposing (rule)
 
+import Elm.Syntax.Declaration as Declaration exposing (Declaration)
 import Elm.Syntax.Expression as Expression exposing (Expression)
 import Elm.Syntax.Node as Node exposing (Node)
 import Review.Fix as Fix
@@ -53,7 +54,8 @@ rule : Rule
 rule =
     Rule.newModuleRuleSchema "NoNameEndingWithColor" ()
         -- Add your visitors
-        |> Rule.withSimpleExpressionVisitor expressionVisitor
+        |> Rule.withSimpleDeclarationVisitor declarationVisitor
+        -- |> Rule.withSimpleExpressionVisitor expressionVisitor
         |> Rule.fromModuleRuleSchema
 
 
@@ -62,28 +64,27 @@ rule =
 -- and report problems that it finds
 
 
-expressionVisitor : Node Expression -> List (Error {})
-expressionVisitor node =
+declarationVisitor : Node Declaration -> List (Error {})
+declarationVisitor node =
     case Node.value node of
-        -- It will look at string literals (like "a", """a""")
-        Expression.FunctionOrValue mod_name str ->
-            -- let
-            --     _ =
-            --         if String.contains "color" str then
-            --             Debug.log "str" str
-            --
-            --         else
-            --             ""
-            -- in
-            if String.endsWith "_color" str then
+        Declaration.FunctionDeclaration { signature, declaration } ->
+            let
+                functionName =
+                    declaration |> Node.value |> .name
+
+                functionNameStr : String
+                functionNameStr =
+                    declaration |> Node.value |> .name |> Node.value
+            in
+            if String.endsWith "_color" functionNameStr then
                 -- Return a single error, describing the problem
                 [ Rule.errorWithFix
-                    { message = "Replace `XYX_color` by `color_XYZ`"
-                    , details = [ "Having names start with color_ makes it easier to search for" ]
+                    { message = "Name should start with color_, not end with it"
+                    , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
                     }
                     -- This is the location of the problem in the source code
-                    (Node.range node)
-                    [ Fix.replaceRangeBy (Node.range node) str ]
+                    (Node.range (declaration |> Node.value |> .name))
+                    [ Fix.replaceRangeBy (Node.range functionName) functionNameStr ]
                 ]
 
             else
@@ -91,3 +92,35 @@ expressionVisitor node =
 
         _ ->
             []
+
+
+
+-- expressionVisitor : Node Expression -> List (Error {})
+-- expressionVisitor node =
+--     case Node.value node of
+--         -- It will look at string literals (like "a", """a""")
+--         Expression.FunctionOrValue mod_name str ->
+--             -- let
+--             --     _ =
+--             --         if String.contains "color" str then
+--             --             Debug.log "str" str
+--             --
+--             --         else
+--             --             ""
+--             -- in
+--             if String.endsWith "_color" str then
+--                 -- Return a single error, describing the problem
+--                 [ Rule.errorWithFix
+--                     { message = "Replace `XYX_color` by `color_XYZ`"
+--                     , details = [ "Having names start with color_ makes it easier to search for" ]
+--                     }
+--                     -- This is the location of the problem in the source code
+--                     (Node.range node)
+--                     [ Fix.replaceRangeBy (Node.range node) str ]
+--                 ]
+--
+--             else
+--                 []
+--
+--         _ ->
+--             []
--- review/tests/NoNameEndingWithColorTest.elm
+++ review/tests/NoNameEndingWithColorTest.elm
@@ -10,15 +10,22 @@ all =
     describe "NoNameEndingWithColor"
         [ test "should report an error when seeing a function or value ending with `_color`" <|
             \() ->
-                """module SomeModule exposing (red_color)
+                """module SomeModule exposing (nothing)
 
+nothing = Nothing
 red_color = 123"""
                     |> Review.Test.run rule
+                    -- |> Review.Test.expectNoErrors
                     |> Review.Test.expectErrors
                         [ Review.Test.error
                             { message = "Name should start with color_, not end with it"
-                            , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            , details = [ "Having names start with color_ makes it easier to search for\nReplace `XYX_color` by `color_XYZ`" ]
+                            , under = "red_color"
                             }
+                            |> Review.Test.whenFixed """module SomeModule exposing (nothing)
+
+nothing = Nothing
+color_red = 123
+"""
                         ]
         ]

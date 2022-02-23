ca45a184d9e4bca6f04a740b3ee6050a2801479d
tankorsmash <tankorsmash@gmail.com>
2022-02-22 20:04:29 -0500

    add ui beep sound without using it

--- src/Sfxr.elm
+++ src/Sfxr.elm
@@ -1,4 +1,4 @@
-port module Sfxr exposing (Model, Msg(..), decodeAndPlaySoundJson, decodeSoundConfig, decodeSoundConfigStr, encodeSoundConfig, init, mineHitConfig, mineSuccessConfig, playSoundConfigWithVol, playerBuyItemConfig, playerSellItemConfig, sfxrOut, subscriptions, suite, update, view)
+port module Sfxr exposing (mouseoverUIBeepConfig, Model, Msg(..), decodeAndPlaySoundJson, decodeSoundConfig, decodeSoundConfigStr, encodeSoundConfig, init, mineHitConfig, mineSuccessConfig, playSoundConfigWithVol, playerBuyItemConfig, playerSellItemConfig, sfxrOut, subscriptions, suite, update, view)
 
 import Element
     exposing
@@ -1746,6 +1746,38 @@ mineSuccessConfig =
         "sample_size": 8
 }"""
 
+
+mouseoverUIBeepConfig : String
+mouseoverUIBeepConfig =
+    """{
+        "oldParams": true,
+        "wave_type": 2,
+        "p_env_attack": 0,
+        "p_env_sustain": 0.03407718326590583,
+        "p_env_punch": 0.412591129522269,
+        "p_env_decay": 0.1463410021953723,
+        "p_base_freq": 0.42253532283411077,
+        "p_freq_limit": 0,
+        "p_freq_ramp": 0,
+        "p_freq_dramp": 0,
+        "p_vib_strength": 0,
+        "p_vib_speed": 0,
+        "p_arp_mod": 0.2937877556257969,
+        "p_arp_speed": 0.5039859435452139,
+        "p_duty": 0,
+        "p_duty_ramp": 0,
+        "p_repeat_speed": 0,
+        "p_pha_offset": 0,
+        "p_pha_ramp": 0,
+        "p_lpf_freq": 1,
+        "p_lpf_ramp": 0,
+        "p_lpf_resonance": 0,
+        "p_hpf_freq": 0,
+        "p_hpf_ramp": 0,
+        "sound_vol": 0.25,
+        "sample_rate": 44100,
+        "sample_size": 8
+}"""
 
 {-| sounds more like a falling or losing sound than a buy-item sound but w/e
 -}

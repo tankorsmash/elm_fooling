2bd8d2d1caf125b5a3f1ecd354a93687f1fdd9f2
tankorsmash <tankorsmash@gmail.com>
2022-03-08 00:34:44 -0500

    mark HideMineAnimation as resting

--- src/ItemShop.elm
+++ src/ItemShop.elm
@@ -2408,7 +2408,7 @@ animator =
                         False
 
                     HideMineAnimation seed ->
-                        True
+                        False
 
                     NoMineAnimation ->
                         True
